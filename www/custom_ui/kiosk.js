ff008fc91e2ccec291c8410f34e8722a62827323
sfgabe <gabe@thisisgabes.com>
2019-08-17 02:21:24 -0400

    Update from backup following reset

--- www/custom_ui/kiosk.js
+++ www/custom_ui/kiosk.js
@@ -6,18 +6,14 @@ if (window.location.href.indexOf('kiosk') > 0)
                     .querySelector('home-assistant-main').shadowRoot
                     .querySelector('app-drawer-layout');
             const huiroot = drawer_layout
-                    .querySelector('partial-panel-resolver')
+                    .querySelector('partial-panel-resolver').shadowRoot
                     .querySelector('ha-panel-lovelace').shadowRoot
-                    .querySelector('hui-root').shadowRoot.querySelector('ha-app-layout');
+                    .querySelector('hui-root').shadowRoot;
             
             const header = huiroot.querySelector('app-header');
-            const toolbar = header.querySelector('app-toolbar');
-            const button_menu = toolbar.querySelector('ha-menu-button');
+            const toolbar = huiroot.querySelector('app-toolbar');
+            const button_menu = toolbar.querySelector('ha-menu-button').shadowRoot;
             const app_drawer = drawer_layout.querySelector('app-drawer');
-
-            if(app_drawer.hasAttribute("opened")){
-                button_menu.querySelector('paper-icon-button').click();
-            }
 
             if (window.location.href.indexOf('show_tabs') > 0) {
                 toolbar.style.display = 'none';
@@ -25,7 +21,9 @@ if (window.location.href.indexOf('kiosk') > 0)
                 header.style.display = 'none';
             }
 
-
+            if(app_drawer.hasAttribute("opened")){
+                button_menu.querySelector('paper-icon-button').click();
+            }
 
             window.dispatchEvent(new Event('resize'));
         }
