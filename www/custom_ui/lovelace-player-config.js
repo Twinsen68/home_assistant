1b31f71b496fe7a963d5da699d48a39a19300917
sfgabe <gabe@thisisgabes.com>
2019-08-17 01:43:48 -0400

    post python 3.6 upgrade configs

--- www/custom_ui/lovelace-player-config.js
+++ www/custom_ui/lovelace-player-config.js
@@ -1,9 +1,3 @@
 setTimeout(function() {
-
-  // Add your bindings here
-  // Example:
-  // LovelacePlayer.bind("media_player.kitchen_dashboard", "ff90349d-4a6eb8b1");
-
-  // Remove this line when you found your Device ID
-  LovelacePlayer.debug();
+  LovelaceFullyKiosk.bind("fb4e04e9-2f0affc3", "light.dashboard_screen", "binary_sensor.dashboard_motion");
 }, 200);
